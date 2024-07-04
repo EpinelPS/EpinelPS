40f78db3010af45a3f86c40ddb06e890fa8b11cb
Mikhail <mishakeys20@gmail.com>
2024-07-03 20:50:12 -0400

    implement GetWallpaperInventory
    lobby settings work now

---
+++ nksrv/LobbyServer/Msgs/User/GetWallpaperInventory.cs
@@ -0,0 +1,22 @@
+﻿using nksrv.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nksrv.LobbyServer.Msgs.User
+{
+    [PacketPath("/user/getwallpaperinventory")]
+    public class GetWallpaperInventory : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqGetWallpaperInventory>();
+
+            var r = new ResGetWallpaperInventory();
+
+            WriteData(r);
+        }
+    }
+}
