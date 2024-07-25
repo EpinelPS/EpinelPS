643d3a5c7c7182f6cea1a9808ec797a56bb854c7
Mikhail <mishakeys20@gmail.com>
2024-07-25 08:54:10 -0400

    Update to game version 123.8.11

---
+++ nksrv/LobbyServer/Msgs/User/GetSpecialLobbySlotData.cs
@@ -0,0 +1,22 @@
+﻿using nksrv.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nksrv.LobbyServer.Msgs.User
+{
+    [PacketPath("/user/speciallobbyslot/get")]
+    public class GetSpecialLobbySlotData : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqGetSpecialLobbySlotData>();
+
+            var response = new ResGetSpecialLobbySlotData();
+            // TODO
+            await WriteDataAsync(response);
+        }
+    }
+}
