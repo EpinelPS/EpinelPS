766a8605b7ffa07541e6fcd3fdf01d9a8540e3f8
Mikhail <mishakeys20@gmail.com>
2024-07-26 14:43:59 -0400

    add GetInterceptData

---
+++ nksrv/LobbyServer/Msgs/Intercept/GetInterceptData.cs
@@ -0,0 +1,25 @@
+﻿using nksrv.Net;
+using nksrv.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nksrv.LobbyServer.Msgs.Intercept
+{
+    [PacketPath("/intercept/get")]
+    public class GetInterceptData : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqGetInterceptData>();
+
+            var response = new ResGetInterceptData();
+
+            // TODO implement
+
+            await WriteDataAsync(response);
+        }
+    }
+}
