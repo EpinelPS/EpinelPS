c2ccfd9fbad3ca153228f1584b14fa8277ad5eee
Atlazs <kevinshley@gmail.com>
2024-09-17 20:32:48 +0200

    Basic manufacturer tower with progress saving

---
+++ EpinelPS/LobbyServer/Msgs/Tower/ClearTower.cs
@@ -0,0 +1,47 @@
+using EpinelPS.Database;
+using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Msgs.Tower
+{
+    [PacketPath("/tower/cleartower")]
+    public class ClearTower : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqClearTower>();
+
+            var response = new ResClearTower();
+            var user = GetUser();
+
+            if (req.BattleResult == 1)
+            {
+                response = CompleteTower(user, req.TowerId);
+            }
+
+            await WriteDataAsync(response);
+        }
+
+        public static ResClearTower CompleteTower(Database.User user, int TowerId)
+        {
+            var response = new ResClearTower();
+
+            // Parse TowerId to get TowerType and FloorNumber
+            int TowerType = (TowerId / 10000) - 1; // For some weird reason the Type here doesn't match up with NetTowerData, thus the -1
+            int FloorNumber = TowerId % 10000;
+
+            // Update user's TowerProgress
+            if (!user.TowerProgress.ContainsKey(TowerType))
+            {
+                user.TowerProgress[TowerType] = FloorNumber;
+            }
+            else if (user.TowerProgress[TowerType] < FloorNumber)
+            {
+                user.TowerProgress[TowerType] = FloorNumber;
+            }
+
+            JsonDb.Save();
+
+            return response;
+        }
+    }
+}
--- EpinelPS/LobbyServer/Msgs/Tower/GetTowerData.cs
+++ EpinelPS/LobbyServer/Msgs/Tower/GetTowerData.cs
@@ -11,6 +11,8 @@ namespace EpinelPS.LobbyServer.Msgs.Tower
 
             var response = new ResGetTowerData();
 
+            var user = GetUser();
+
             // TODO: Load remain count for these
             var t0 = new NetTowerData() { Type = 1, RemainCount = 3 };
             var t1 = new NetTowerData() { Type = 2, RemainCount = 3 };
@@ -24,6 +26,17 @@ namespace EpinelPS.LobbyServer.Msgs.Tower
             t2.Schedules.Add(new NetSchedule() { DayOfWeek = new() { DayOfWeeks = { 0, 3, 6 }, StartTime = 720000000000, Duration = 863990000000 } });
             t3.Schedules.Add(new NetSchedule() { DayOfWeek = new() { DayOfWeeks = { 2, 6 }, StartTime = 720000000000, Duration = 863990000000 } });
             t4.Schedules.Add(new NetSchedule() { AllTime = new() });
+
+            if (user.TowerProgress.TryGetValue(1, out int floor1))
+                t0.Floor = floor1;
+            if (user.TowerProgress.TryGetValue(2, out int floor2))
+                t1.Floor = floor2;
+            if (user.TowerProgress.TryGetValue(3, out int floor3))
+                t2.Floor = floor3;
+            if (user.TowerProgress.TryGetValue(4, out int floor4))
+                t3.Floor = floor4;
+            if (user.TowerProgress.TryGetValue(5, out int floor5))
+                t4.Floor = floor5;
 
             response.Data.Add(t0);
             response.Data.Add(t1);
