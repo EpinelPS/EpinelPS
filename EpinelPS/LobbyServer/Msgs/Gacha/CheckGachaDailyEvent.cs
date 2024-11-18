2a089a035bec6464f97a2b30c3cb6e256ac6e0bf
SELEKCJONER <majsetr.od.peceta@gmail.com>
2024-11-18 22:35:45 +0100

    Add 1x free gacha
    Copies regular 1x behavior

--- EpinelPS/LobbyServer/Msgs/Gacha/CheckGachaDailyEvent.cs
+++ EpinelPS/LobbyServer/Msgs/Gacha/CheckGachaDailyEvent.cs
@@ -14,14 +14,14 @@ namespace EpinelPS.LobbyServer.Msgs.Gacha
             response.FreeCount = 1;
             response.EventData = new NetEventData()
             {
-                Id = 70070,
-                EventSystemType = 6,
+                Id = 80004,
+                EventSystemType = 21,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
                 EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks,
+                EventDisableDate = DateTime.Now.AddDays(20).Ticks
             };
-			// this is net event data i think it should be the same as in list events
+			// this is net event data i think it should be the same as in list events and get joined event only for free pull gacha event 
 
             await WriteDataAsync(response);
         }
