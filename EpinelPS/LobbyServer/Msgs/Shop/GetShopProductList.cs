1e7de0ef8b9810e8014dddbe98bdc2fafb9c92a0
Mikhail <mishakeys20@gmail.com>
2024-12-20 15:58:43 -0500

    Revert "Update GetShopProductList.cs"
    This reverts commit 80307453376b610a254aafd69346f9765ccb3021.

--- EpinelPS/LobbyServer/Msgs/Shop/GetShopProductList.cs
+++ EpinelPS/LobbyServer/Msgs/Shop/GetShopProductList.cs
@@ -1,5 +1,4 @@
-﻿using EpinelPS.StaticInfo;
-using EpinelPS.Utils;
+﻿using EpinelPS.Utils;
 
 namespace EpinelPS.LobbyServer.Msgs.Shop
 {
@@ -10,18 +9,6 @@ namespace EpinelPS.LobbyServer.Msgs.Shop
         {
             var req = await ReadData<ReqShopProductList>();
             var response = new ResShopProductList();
-
-            response.Shops.Add(new NetShopProductData()
-            {
-               FreeRenewCount = 1,
-               ShopTid =  1,
-               ShopCategory = (int)ShopCategoryType.Normal,
-               RenewAt = DateTime.Now.AddDays(1).Ticks,
-               NextRenewAt = DateTime.Now.AddDays(1).Ticks,
-               RenewCount = 0
-            });
-
-            response.Shops[0].List.Add(new NetShopProductInfoData() { BuyCount = 0, ProductId = 201, CorporationType = 0});
 
             await WriteDataAsync(response);
         }
