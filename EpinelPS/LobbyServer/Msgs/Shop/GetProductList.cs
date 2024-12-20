5391a2da3a166c70b76271df18d464ececb41dcb
Mikhail <mishakeys20@gmail.com>
2024-12-20 15:58:58 -0500

    Update GetProductList.cs

--- EpinelPS/LobbyServer/Msgs/Shop/GetProductList.cs
+++ EpinelPS/LobbyServer/Msgs/Shop/GetProductList.cs
@@ -8,36 +8,36 @@ namespace EpinelPS.LobbyServer.Msgs.Shop
     {
         protected override async Task HandleAsync()
         {
-            try
+            var x = await ReadData<ReqGetJupiterProductList>();
+
+            var response = new ResGetJupiterProductList();
+            foreach (var item in x.ProductIdList)
             {
-                var x = await ReadData<ReqGetJupiterProductList>();
+                // TODO: Optimize this!
+                var product = GameData.Instance.mediasProductTable.Where(x => x.Key == item);
 
-                var response = new ResGetJupiterProductList();
-                foreach (var item in x.ProductIdList)
+                if (product.Any())
                 {
-                    // TODO: Optimize this!
-                    var product = GameData.Instance.mediasProductTable.Where(x => x.Key == item);
-
-                    if (product.Any())
+                    // Example:
+                    // Midas RequestGetLocalPriceAsync res ProductId = com.proximabeta.nikke.costumegacha11_02, Price = 3.99, MicroPrice = 3990000, CurrencyCode = USD, CurrencySymbol = $
+                    MidasProductRecord? record = product.FirstOrDefault().Value;
+                    if (record != null)
                     {
-                        // Example:
-                        // Midas RequestGetLocalPriceAsync res ProductId = com.proximabeta.nikke.costumegacha11_02, Price = 3.99, MicroPrice = 3990000, CurrencyCode = USD, CurrencySymbol = $
-                        MidasProductRecord? record = product.FirstOrDefault().Value;
-
-                        long microPrice = (long)(double.Parse(record.cost) * 1000000);
+                        if(!double.TryParse(record.cost, out double price))
+                        {
+                            Console.WriteLine("Failed to parse " + record.cost+" Cash shop will not work probably");
+                        }
+                        
+                        long microPrice = (long)(price * 1000000);
                         response.ProductInfoList.Add(new NetJupiterProductInfo() { CurrencyCode = "USD", CurrencySymbol = "$", MicroPrice = microPrice, Price = record.cost, ProductId = item });
                     }
-                    else
-                    {
-                        Console.WriteLine("Missing!!!! " + item);
-                    }
                 }
-                await WriteDataAsync(response);
+                else
+                {
+                    Console.WriteLine("Missing!!!! " + item);
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error while handling GetProductList request. Have you replaced sodium library?" + ex);
-            }
+            await WriteDataAsync(response);
         }
     }
 }
