5ff6264e21dfe6127940d7eafc21408b41427fa2
SELEKCJONER <majsetr.od.peceta@gmail.com>
2024-12-25 15:03:57 +0100

    Very basic implementation of mission pass

---
+++ EpinelPS/LobbyServer/Pass/buyrank.cs
@@ -0,0 +1,18 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Pass
+{
+    [PacketPath("/pass/buyrank")]
+    public class BuyPassRank : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqBuyPassRank>(); //fields "PassId", "TargetPassRank"
+			
+            var response = new ResBuyPassRank(); // fields "PassRank", "PassPoint", "Currencies"
+
+           
+		   await WriteDataAsync(response);
+        }
+    }
+}
---
+++ EpinelPS/LobbyServer/Pass/obtainreward.cs
@@ -0,0 +1,18 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Pass
+{
+    [PacketPath("/pass/obtainreward")]
+    public class ObtainPassReward : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqObtainPassReward>(); //fields "PassId", "PassRank"
+			
+            var response = new ResObtainPassReward(); // field Reward
+
+           
+		   await WriteDataAsync(response);
+        }
+    }
+}
