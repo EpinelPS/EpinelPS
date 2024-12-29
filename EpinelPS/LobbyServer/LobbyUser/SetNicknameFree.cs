1cb1e3467492521e1ec63d1bcfb492fffc5a5e93
Mikhail <mishakeys20@gmail.com>
2024-12-29 10:44:34 -0500

    add setnicknamefree

---
+++ EpinelPS/LobbyServer/LobbyUser/SetNicknameFree.cs
@@ -0,0 +1,21 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.LobbyUser
+{
+    [PacketPath("/user/setnicknamefree")]
+    public class SetNicknameFree : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqSetNicknameFree>();
+            var user = GetUser();
+            user.Nickname = req.Nickname;
+
+            var response = new ResSetNicknameFree();
+            response.Result = SetNicknameResult.SetNicknameResultOkay;
+            response.Nickname = req.Nickname;
+
+            await WriteDataAsync(response);
+        }
+    }
+}
