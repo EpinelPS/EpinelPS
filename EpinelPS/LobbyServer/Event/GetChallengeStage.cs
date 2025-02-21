07fab687e782753a08d27c1d045648127981b214
Mikhail <mishakeys20@gmail.com>
2025-02-21 18:19:52 -0500

    Add event/challengestage/get

---
+++ EpinelPS/LobbyServer/Event/GetChallengeStage.cs
@@ -0,0 +1,22 @@
+using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Event
+{
+    [PacketPath("/event/challengestage/get")]
+    public class GetChallengeStage : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ResChallengeEventStageData>();
+            var user = GetUser();
+
+            var response = new ResChallengeEventStageData();
+            response.RemainTicket = 3;
+            response.TeamData = user.UserTeams[1];
+
+            // TODO implement properly
+  
+            await WriteDataAsync(response);
+        }
+    }
+}
