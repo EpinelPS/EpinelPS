4e568390edbd6a55f3e4229878288346069078c9
Mikhail Tyukin <mishakeys20@gmail.com>
2025-06-24 21:54:47 +0400

    Create Counsel.cs

---
+++ EpinelPS/LobbyServer/Character/Counsel.cs
@@ -0,0 +1,21 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Character
+{
+    [PacketPath("/character/attractive/counsel")]
+    public class Counsel : LobbyMsgHandler
+    {
+        protected override async Task HandleAsync()
+        {
+            var req = await ReadData<ReqCharacterCounsel>();
+            var user = GetUser();
+
+            ResCharacterCounsel response = new();
+            response.Attractive = new();
+            response.Exp = new();
+
+            // TODO
+            await WriteDataAsync(response);
+        }
+    }
+}
