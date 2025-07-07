10f2a9ebf493475ba9f77b9951c779cb2597b554
Mikhail Tyukin <mishakeys20@gmail.com>
2025-07-07 14:20:47 +0400

    update event data

--- EpinelPS/LobbyServer/Event/ListEvents.cs
+++ EpinelPS/LobbyServer/Event/ListEvents.cs
@@ -10,23 +10,23 @@ namespace EpinelPS.LobbyServer.Event
         {
             var req = await ReadData<ReqGetEventList>();
 
-            //types are defined in EventTypes.cs
+            // types are defined in EventTypes.cs
             var response = new ResGetEventList();
 
-            // stellar blade event
+            // OuteR: Automata event
             response.EventList.Add(new NetEventData()
             {
-                Id = 82100,
+                Id = 82300,
                 EventSystemType = FieldHubEvent,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
                 EventEndDate = DateTime.Now.AddDays(20).Ticks,
                 EventDisableDate = DateTime.Now.AddDays(20).Ticks
             });
-           response.EventList.Add(new NetEventData()
+            response.EventList.Add(new NetEventData()
             {
-                Id = 82101,
-                EventSystemType = CE006BossChallengeMiniGame,
+                Id = 82303,
+                EventSystemType = CE002MiniGame,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
                 EventEndDate = DateTime.Now.AddDays(20).Ticks,
@@ -34,35 +34,7 @@ namespace EpinelPS.LobbyServer.Event
             });
             response.EventList.Add(new NetEventData()
             {
-                Id = 81903,
-                EventSystemType = LoginEvent,
-                EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
-                EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
-                EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks
-            });
-            response.EventList.Add(new NetEventData()
-            {
-                Id = 40070,
-                EventSystemType = StoryEvent,
-                EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
-                EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
-                EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks
-            });
-            response.EventList.Add(new NetEventData()
-            {
-                Id = 40071,
-                EventSystemType = StoryEvent,
-                EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
-                EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
-                EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks
-            });
-            
-            response.EventList.Add(new NetEventData()
-            {
-                Id = 82102,
+                Id = 82302,
                 EventSystemType = ShopEvent,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
@@ -71,34 +43,7 @@ namespace EpinelPS.LobbyServer.Event
             });
             response.EventList.Add(new NetEventData()
             {
-                Id = 82104,
-                EventSystemType = FieldCollectEvent,
-                EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
-                EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
-                EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks
-            });
-            response.EventList.Add(new NetEventData()
-            {
-                Id = 82105,
-                EventSystemType = LoginEvent,
-                EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
-                EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
-                EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks
-            });
-            response.EventList.Add(new NetEventData()
-            {
-                Id = 30068,
-                EventSystemType = CooperationEvent,
-                EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
-                EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
-                EventEndDate = DateTime.Now.AddDays(20).Ticks,
-                EventDisableDate = DateTime.Now.AddDays(20).Ticks
-            });
-            response.EventList.Add(new NetEventData()
-            {
-                Id = 40079,
+                Id = 40082,
                 EventSystemType = StoryEvent,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
@@ -107,7 +52,7 @@ namespace EpinelPS.LobbyServer.Event
             });
             response.EventList.Add(new NetEventData()
             {
-                Id = 40080,
+                Id = 40083,
                 EventSystemType = StoryEvent,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
@@ -116,7 +61,7 @@ namespace EpinelPS.LobbyServer.Event
             });
             response.EventList.Add(new NetEventData()
             {
-                Id = 60079,
+                Id = 60082,
                 EventSystemType = ChallengeModeEvent,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
@@ -125,7 +70,7 @@ namespace EpinelPS.LobbyServer.Event
             });
             response.EventList.Add(new NetEventData()
             {
-                Id = 100029,
+                Id = 100031,
                 EventSystemType = EventPass,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
@@ -134,8 +79,8 @@ namespace EpinelPS.LobbyServer.Event
             });
             response.EventList.Add(new NetEventData()
             {
-                Id = 100030,
-                EventSystemType = EventPass,
+                Id = 82304,
+                EventSystemType = LoginEvent,
                 EventVisibleDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(7)).Ticks,
                 EventStartDate = DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)).Ticks,
                 EventEndDate = DateTime.Now.AddDays(20).Ticks,
