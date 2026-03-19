3e1d684c6f14869b0cca75ac3fa3432fa68a5793
Mikhail Tyukin <mishakeys20@gmail.com>
2026-03-19 20:58:42 +0900

    Fix login

---
+++ EpinelPS/LobbyServer/Event/Minigame/ScenarioChoice/GetRedDot.cs
@@ -0,0 +1,22 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Event.Minigame.ScenarioChoiceis;
+
+[PacketPath("/event/minigame/scenariochoice/reddot")]
+public class GetRedDot : LobbyMsgHandler
+{
+    protected override async Task HandleAsync()
+    {
+        ReqExistScenarioChoiceRedDotData req = await ReadData<ReqExistScenarioChoiceRedDotData>();
+        User user = GetUser();
+
+        ResExistScenarioChoiceRedDotData response = new()
+        {
+            
+        };
+
+        // TODO implement properly
+
+        await WriteDataAsync(response);
+    }
+}
