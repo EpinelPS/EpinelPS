a4a6878362b4fa5c6ccc0eff7f3e862473a31504
Mikhail Tyukin <mishakeys20@gmail.com>
2026-04-19 11:49:15 -0400

    hide unimplemented features in admin panel, add few more requests, show events in event page

---
+++ EpinelPS/LobbyServer/Event/Minigame/RebuildEden/GetBadgeData.cs
@@ -0,0 +1,19 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Event.Minigame.RebuildEden;
+
+[PacketPath("/event/minigame/rebuildeden/getbadge")]
+public class GetBadgeData : LobbyMsgHandler
+{
+    protected override async Task HandleAsync()
+    {
+        ReqGetRebuildEdenBadge req = await ReadData<ReqGetRebuildEdenBadge>();
+        User user = GetUser();
+
+        ResGetRebuildEdenBadge response = new();
+
+        // TODO implement
+
+        await WriteDataAsync(response);
+    }
+}
---
+++ EpinelPS/LobbyServer/Sidestory/GetRecommendationRedDot.cs
@@ -0,0 +1,19 @@
+﻿using EpinelPS.Utils;
+
+namespace EpinelPS.LobbyServer.Sidestory;
+
+[PacketPath("/eventfield/sidestory/recommendation/reddot")]
+public class GetRecommendationRedDot : LobbyMsgHandler
+{
+    protected override async Task HandleAsync()
+    {
+        ReqSideStoryRecommendationReddotData req = await ReadData<ReqSideStoryRecommendationReddotData>();
+        User user = GetUser();
+
+        ResSideStoryRecommendationReddotData response = new();
+
+        // TODO
+
+        await WriteDataAsync(response);
+    }
+}
