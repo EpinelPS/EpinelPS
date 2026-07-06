fc5988fe3898db05d1879b1ebe8e4568322fa06f
Mikhail Tyukin <mishakeys20@gmail.com>
2026-07-06 09:28:53 -0400

    fix user registration regression

--- EpinelPS/Models/GameUser.cs
+++ EpinelPS/Models/GameUser.cs
@@ -6,7 +6,7 @@ namespace EpinelPS.Models;
 public class GameUser
 {
     public ulong ID { get; set; }
-    public string Nickname { get; set; }
+    public string Nickname { get; set; } = "Player";
     public DateTime LastAction { get; set;}
     public ICollection<TriggerModelNew> Triggers { get; set; } = new List<TriggerModelNew>();
 }
