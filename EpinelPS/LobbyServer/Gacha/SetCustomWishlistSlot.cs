32faf94b536fc4665d11392e2fe762002c655d02
sleepless-wombat <chrlouis64@bigpond.com>
2026-07-29 22:44:01 +1000

    Restored allmsgs to its original state and added a custom parsing function for NetCustomPackageSetupData
    - Restored allmsgs.cs to its original state.
    - Added a StreamUtils class that holds a parser function for the NetCustomPackageSetupData request.
    - Updated the wishlist save endpoint to use the custom parser (only if the DataList received is empty). This ensures that if the list gets filled in the future, the code will be able to use it.

--- EpinelPS/LobbyServer/Gacha/SetCustomWishlistSlot.cs
+++ EpinelPS/LobbyServer/Gacha/SetCustomWishlistSlot.cs
@@ -18,9 +18,27 @@ public class SetCustomWishlistSlot : LobbyMessage
 
         // Add the characters to the user wishlist. For now, I will assume that the game sent the right data.
         // TODO: Check that all ids are correct and only from the allowed pull list
-         User user = GetUser();
+        User user = GetUser();
 
-        var wishlist = req.DataList.Select(d => d.SlotList.FirstOrDefault(-1)).Where(id => id != -1).ToList();
+        List<int> wishlist = req.DataList.Select(d => d.SlotList.FirstOrDefault(-1)).Where(id => id != -1).ToList();
+
+        // If the list is empty, try to parse the body directly.
+        if (wishlist.Count == 0)
+        {
+            ctx.Request.Body.Position = 0;
+            try
+            {
+                var reqContent = StreamUtils.ParseNetCustomPackageSetupData(ctx.Request.Body);
+                wishlist = reqContent.Select(l => l.SlotList[0]).ToList();
+            }
+            catch (Exception ex)
+            {
+                // Can't read the request, fail gracefully
+                Logging.WriteLine("Could not read [ReqSetCustomPackageSlot]");
+            }
+        }
+
+        // Parse the request directly if the request is empty
 
         // The wishlist must have 20 characters to be valid, otherwise clear it.
         if (wishlist.Count == 20)
@@ -30,12 +48,15 @@ public class SetCustomWishlistSlot : LobbyMessage
         else
         {
             user.CharacterWishlist.Clear();
-        }        
+        }
+
 
         Logging.WriteLine($"[SetCustomWishlistSlot] Updated wishlist for user {user.ID}: [{string.Join(", ", user.CharacterWishlist)}]");
 
+        // Save
+        JsonDb.Save();
 
         await WriteDataAsync(req);
     }
-    
+
 }
---
+++ EpinelPS/Utils/StreamUtils.cs
@@ -0,0 +1,114 @@
+﻿namespace EpinelPS.Utils;
+
+
+/// <summary>
+/// This class contain utility functions for stream manipulation
+/// </summary>
+public class StreamUtils
+{
+    /// <summary>
+    /// Alternative parser for NetCustomPackageSetupData.
+    /// At the time of writing, the DataList in the ReqSetCustomPackageSlot request is always empty.
+    /// However, the data is present in the request body. This function extracts the content if available.
+    /// This only reads the relevant data for the wishlist selection
+    /// </summary>
+    public static List<NetCustomPackageSetupData> ParseNetCustomPackageSetupData(Stream stream)
+    {
+        var result = new List<NetCustomPackageSetupData>();
+        using var reader = new BinaryReader(stream);
+
+        // Process the protobuf model manually
+        // We are going to follow the official definition of the NetCustomPackageSetupData message,
+        // but assume that the data_list is field 3 instead of 1;
+        while (stream.Position < stream.Length)
+        {
+            byte tag = reader.ReadByte();
+            int fieldNumber = tag >> 3;
+            int wireType = tag & 0x07;
+            
+            // Field 3 (0x1A): repeated NetCustomPackageSetupData data_list (wire type 2)
+            if (fieldNumber == 3 && wireType == 2)
+            {
+                int length = (int)ReadVarInt(reader);
+                int endOfMessage = (int)stream.Position + length;
+
+                var data = ParseNetCustomPackageSetupDataFromStream(reader, endOfMessage);
+                result.Add(data);
+
+                stream.Position = endOfMessage;
+            }
+            else
+            {
+                // Unknown fields, skip
+                SkipField(reader, wireType);
+            }
+        }
+
+        return result;
+    }
+
+    private static NetCustomPackageSetupData ParseNetCustomPackageSetupDataFromStream(BinaryReader reader, int endOfMessage)
+    {
+        var data = new NetCustomPackageSetupData();
+
+        // Process the request stream
+        while (reader.BaseStream.Position < endOfMessage)
+        {
+            byte tag = reader.ReadByte();
+            int fieldNumber = tag >> 3;
+            int wireType = tag & 0x07;
+
+            // custom_package_shop_tid
+            if (fieldNumber == 1 && wireType == 0)
+                data.CustomPackageShopTid = (int)ReadVarInt(reader);
+            // slot_list
+            else if (fieldNumber == 2 && wireType == 0)
+                data.SlotList.Add((int)ReadVarInt(reader));
+            // Unknown field
+            else             
+                SkipField(reader, wireType);
+        }
+
+        return data;
+    }
+      
+    /// <summary>
+    /// Read a varint type (ulong) variable from a stream
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <returns></returns>
+    private static ulong ReadVarInt(BinaryReader reader)
+    {
+        ulong result = 0;
+        int shift = 0;
+        while (true)
+        {
+            byte b = reader.ReadByte();
+            result |= (ulong)(b & 0x7F) << shift;
+            if ((b & 0x80) == 0) break;
+            shift += 7;
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Skip a "field" base on the wire type. Only handling 0 (int) and 2 (lists) here but more types can be added later on.
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="wireType"></param>
+    /// <exception cref="InvalidOperationException"></exception>
+    private static void SkipField(BinaryReader reader, int wireType)
+    {
+        switch (wireType)
+        {
+            case 0: ReadVarInt(reader); break;
+            case 2:
+                int len = (int)ReadVarInt(reader);
+                reader.BaseStream.Position += len;
+                break;
+            case 5: reader.ReadUInt32(); break;
+            default: throw new InvalidOperationException($"Unsupported wire type: {wireType}");
+        }
+    }
+
+}
