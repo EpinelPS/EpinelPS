d77bcc7f7fab7351a4c0674a4fd0a15f12fd38d4
Muhammad Adeel Abid <digitaladeel@proton.me>
2026-08-20 22:17:22 +0500

    Default Profile Cards fix

--- EpinelPS/LobbyServer/LobbyUser/GetProfileCardPossession.cs
+++ EpinelPS/LobbyServer/LobbyUser/GetProfileCardPossession.cs
@@ -12,24 +12,26 @@ public class GetProfileCardPossession : LobbyMessage
         ResProfileCardObjectList response = new();
 
         var pcoTable = GameData.Instance.ProfileCardObjectTable.Values.ToList();
-        if (user.ProfileCardsData.Count > 0)
+        var rareCards = pcoTable.Where(x => x.GradeType == ProfileCardObjectGradeType.R)
+                .Select(c => c.Id).ToList();
+
+        user.ProfileCardsData.AddRange(rareCards.Except(user.ProfileCardsData));
+        user.ProfileCardsData.ForEach(c =>
         {
-            user.ProfileCardsData.ForEach(c =>
+            GameData.Instance.ProfileCardObjectTable.TryGetValue(c, out var card);
+            switch (card.ObjectType)
             {
-                GameData.Instance.ProfileCardObjectTable.TryGetValue(c, out var card);
-                switch (card.ObjectType)
-                {
-                    case ObjectType.BackGround:
-                        response.BackgroundIds.Add(c);
-                        break;
-                    case ObjectType.Sticker:
-                        response.StickerIds.Add(c);
-                        break;
-                    default:
-                        break;
-                }
-            });
-        }
+                case ObjectType.BackGround:
+                    response.BackgroundIds.Add(c);
+                    break;
+                case ObjectType.Sticker:
+                    response.StickerIds.Add(c);
+                    break;
+                default:
+                    break;
+            }
+        });
+
         await WriteDataAsync(response);
     }
 }
